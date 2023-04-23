e7866699dcb87eba3819f6cd5195af57fbbbf11b
thr3a <thr3a@example.com>
2023-04-23 23:45:29 +0900

    add: growi-es

--- face_recognition/README.md
+++ face_recognition/README.md
@@ -3,3 +3,10 @@
 ```
 docker run --rm -v `pwd`:/app -w /app thr3a/face_recognition --image image.jpg --extension png
 ```
+
+
+一括バージョン
+
+```
+find . -name "*.jpg" -exec docker run --rm -it -v `pwd`:/app -w /app thr3a/face_recognition --image {} --extension png --padding 180 \;
+```
--- face_recognition/index.py
+++ face_recognition/index.py
@@ -1,6 +1,7 @@
 import argparse
 import cv2
 import dlib
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--image", help="Input image file name")
@@ -22,11 +23,26 @@ faces = face_detector(img, 1)
 
 # 各顔に対して処理を行う
 for (i, face) in enumerate(faces):
-    # 10pxのpaddingを加えて正方形に切り抜く
-    x1, y1, x2, y2 = face.left() - padding, face.top() - padding, face.right() + padding, face.bottom() + padding
+    # 顔の位置を取得する
+    x1, y1, x2, y2 = face.left(), face.top(), face.right(), face.bottom()
 
-    # 縦512px 横512pxの画像にリサイズする
-    face_img = cv2.resize(img[y1:y2, x1:x2], (512, 512))
+    # paddingを加えて正方形に切り抜く
+    face_width = x2 - x1
+    face_height = y2 - y1
+    if face_width > face_height:
+        y1 = y1 - int((face_width - face_height) / 2)
+        y2 = y2 + int((face_width - face_height) / 2)
+    else:
+        x1 = x1 - int((face_height - face_width) / 2)
+        x2 = x2 + int((face_height - face_width) / 2)
+    x1 = max(0, x1 - padding)
+    y1 = max(0, y1 - padding)
+    x2 = min(img.shape[1], x2 + padding)
+    y2 = min(img.shape[0], y2 + padding)
 
-    face_image_path = "{}_face_{}.{}".format(image_path.split('.')[0], i, extension)
+    # 顔を切り抜いてリサイズする
+    face_img = img[y1:y2, x1:x2]
+    face_img = cv2.resize(face_img, (512, 512))
+
+    face_image_path = "{}_face_{}.{}".format(Path(image_path).stem, i, extension)
     cv2.imwrite(face_image_path, face_img)
